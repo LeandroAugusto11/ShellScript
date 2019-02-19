1f6f12daf061bfcf6964ffb7c9ce4925ec03a222
LeandroAugusto11 <43184863+LeandroAugusto11@users.noreply.github.com>
2019-02-19 16:06:38 -0300

    Update desafio.sh

--- desafio.sh
+++ desafio.sh
@@ -37,10 +37,29 @@ touch /tmp/leandro/leandro.txt
 echo "Pronto" 
 fi
 cat << "EOF"
-______ ____ ____ _ __ _ |_ _ \ |_ _| |_ _|(_) | ] | | | |_) | .---. _ .--..--. \ \ / / __ _ .--. .--.| | .--. | | | __&apos;. / /__\\[ `.-. .-. | \ \ / / [ | [ `.-. |/ /&apos;`\&apos; |/ .&apos;`\ \| | _| |__) ||\__., | | | | | | \ &apos; / | | | | | || \__/ || \__. ||_| |_______/ &apos;.__.&apos;[___||__||__] \_/ [___][___||__]&apos;.__.;__]&apos;.__.&apos; (_) 
+dddddddd                 
+BBBBBBBBBBBBBBBBB                                                    VVVVVVVV           VVVVVVVV iiii                               d::::::d                 
+B::::::::::::::::B                                                   V::::::V           V::::::Vi::::i                              d::::::d                 
+B::::::BBBBBB:::::B                                                  V::::::V           V::::::V iiii                               d::::::d                 
+BB:::::B     B:::::B                                                 V::::::V           V::::::V                                    d:::::d                  
+  B::::B     B:::::B    eeeeeeeeeeee       mmmmmmm    mmmmmmm         V:::::V           V:::::Viiiiiiinnnn  nnnnnnnn        ddddddddd:::::d    ooooooooooo   
+  B::::B     B:::::B  ee::::::::::::ee   mm:::::::m  m:::::::mm        V:::::V         V:::::V i:::::in:::nn::::::::nn    dd::::::::::::::d  oo:::::::::::oo 
+  B::::BBBBBB:::::B  e::::::eeeee:::::eem::::::::::mm::::::::::m        V:::::V       V:::::V   i::::in::::::::::::::nn  d::::::::::::::::d o:::::::::::::::o
+  B:::::::::::::BB  e::::::e     e:::::em::::::::::::::::::::::m         V:::::V     V:::::V    i::::inn:::::::::::::::nd:::::::ddddd:::::d o:::::ooooo:::::o
+  B::::BBBBBB:::::B e:::::::eeeee::::::em:::::mmm::::::mmm:::::m          V:::::V   V:::::V     i::::i  n:::::nnnn:::::nd::::::d    d:::::d o::::o     o::::o
+  B::::B     B:::::Be:::::::::::::::::e m::::m   m::::m   m::::m           V:::::V V:::::V      i::::i  n::::n    n::::nd:::::d     d:::::d o::::o     o::::o
+  B::::B     B:::::Be::::::eeeeeeeeeee  m::::m   m::::m   m::::m            V:::::V:::::V       i::::i  n::::n    n::::nd:::::d     d:::::d o::::o     o::::o
+  B::::B     B:::::Be:::::::e           m::::m   m::::m   m::::m             V:::::::::V        i::::i  n::::n    n::::nd:::::d     d:::::d o::::o     o::::o
+BB:::::BBBBBB::::::Be::::::::e          m::::m   m::::m   m::::m              V:::::::V        i::::::i n::::n    n::::nd::::::ddddd::::::ddo:::::ooooo:::::o
+B:::::::::::::::::B  e::::::::eeeeeeee  m::::m   m::::m   m::::m               V:::::V         i::::::i n::::n    n::::n d:::::::::::::::::do:::::::::::::::o
+B::::::::::::::::B    ee:::::::::::::e  m::::m   m::::m   m::::m                V:::V          i::::::i n::::n    n::::n  d:::::::::ddd::::d oo:::::::::::oo 
+BBBBBBBBBBBBBBBBB       eeeeeeeeeeeeee  mmmmmm   mmmmmm   mmmmmm                 VVV           iiiiiiii nnnnnn    nnnnnn   ddddddddd   ddddd   ooooooooooo   
+                                                                                                                                                             
+                                                                                                                                                             
+                                                                                                                                                       
 EOF
-usuario=$(who | awk &apos;{print $1}&apos;)
-ano=$(date +"%Y")
+USUARIO=$(who | awk &apos;{print $1}&apos;)
+ANO=$(date +"%Y")
 
 read -p "Digite a sua idade: " idade;
 
